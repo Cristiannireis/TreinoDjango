5e753f42625c583b2a13c50048e13e477ba0f6ea
Cristiane Reis <anni.nanne@gmail.com>
2022-11-28 22:59:40 -0300

    desenvolvendo o treino

--- projeto1/app1/views.py
+++ projeto1/app1/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from .forms import UserForm
 
 
 def index(request):
     return render(request, 'user/index.html')
+
+def create(request):
+
+    if request.method == 'GET':
+        form = UserForm()
+
+        context = {
+            'form': form,
+        }
+    return render(request, 'user/criar.html', context=context)
+
+
 
 
 # Create your views here.
